45deec8ae1b04e9cbabe9e834a72f85978d8af56
dozoq <Sebastian.malek@fajne.to>
2022-05-06 16:47:23 +0200

    init

---
+++ Vehicle/IEngine.cs
@@ -0,0 +1,13 @@
+﻿namespace Vehicle
+{
+    public interface IEngine
+    {
+        public int      Power { get; protected set; }
+        public FuelType Fuel  { get; protected set; }
+    }
+
+    public enum FuelType
+    {
+        Gasolin, Oil, LPG, Electricity
+    }
+}
---
+++ Vehicle/IVehicle.cs
@@ -0,0 +1,18 @@
+﻿namespace Vehicle
+{
+    public interface IVehicle
+    {
+        public       VehicleState State { get; protected set; }
+        public       float        Speed { get; protected set; }
+        
+        public void Move()
+        {
+            
+        }
+    }
+
+    public enum VehicleState
+    {
+        Moving, Staying
+    }
+}
